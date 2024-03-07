ae681f05fd228618f8fd2eb69ae2713dc65b6a22
AAKudryavtsev <a.kudryavcev@dev.vtb.ru>
2024-03-07 12:01:03 +0300

    1234567891011

--- src/main/java/study/stepup/lab4/loader/LoaderFromFiles.java
+++ src/main/java/study/stepup/lab4/loader/LoaderFromFiles.java
@@ -22,35 +22,30 @@ public class LoaderFromFiles implements Loader{
 
         List<DataType> dataFromFiles = new ArrayList<>();
 
-        if (files.length > 0) {
-            for (File file : files) {
-                try (Scanner scan = new Scanner(file)) {
-                    while (scan.hasNext()) {
-                        String dataLine = scan.nextLine();
-                        String[] words = dataLine.split("\\s+");
-                        DataType dataType = new DataType();
-                        //проверяем что строка состоит из 7 слов разделенных пробелами. Если нет, то это не наш формат
-                        if (words.length == 7) {
-                            //try {
-                                dataType.setFileName(file.getName());
-                                dataType.setLogin(words[0]);
-                                dataType.setSurname(words[1]);
-                                dataType.setName(words[2]);
-                                dataType.setPatr(words[3]);
-                                dataType.setAccesDate(words[4] + " " + words[5]);
-                                dataType.setApplication(words[6]);
+        for (File file : files) {
+            try (Scanner scan = new Scanner(file)) {
+                while (scan.hasNext()) {
+                    String dataLine = scan.nextLine();
+                    //в задании указан конкретный формат с пробелами. Сделал парсинг именно так.
+                    String[] words = dataLine.split("\\s+");
+                    DataType dataType = new DataType();
+                    //проверяем что строка состоит из 7 слов разделенных пробелами. Если нет, то это не наш формат
+                    //Если есть другие ошибки, то они будут реальными ошибками - пусть ломается. надо разбираться
+                    if (words.length == 7) {
+                        dataType.setFileName(file.getName());
+                        dataType.setLogin(words[0]);
+                        dataType.setSurname(words[1]);
+                        dataType.setName(words[2]);
+                        dataType.setPatr(words[3]);
+                        dataType.setAccesDate(words[4] + " " + words[5]);
+                        dataType.setApplication(words[6]);
 
-                                dataFromFiles.add(dataType);
-
-//                            } catch (Exception ex) {
-//                                continue;
-//                            }
-                        }
-
+                        dataFromFiles.add(dataType);
                     }
-                } catch (Exception exception) {
-                    throw exception;
+
                 }
+            } catch (Exception exception) {
+                throw exception;
             }
         }
         return dataFromFiles;
